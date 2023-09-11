dd8b9ee2963949c673eda0828f3d213c974faa4d
seanli01 <seanmingli2001@gmail.com>
2023-09-11 12:12:32 -0400

    added Reduce.java and Multiples.java

---
+++ src/Multiples.java
@@ -0,0 +1,14 @@
+public class Multiples {
+
+    public static void main(String args[]){
+        for(int i =1; i <=1000; i++){
+            if (i%3 ==0){
+                System.out.println(i);
+            }
+            else if (i%5==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
